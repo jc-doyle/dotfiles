50bf1012a507936bf6a60f35a759339dead4ac3f
JC-Doyle <doyle.jonty@gmail.com>
2021-07-19 23:34:15 +0200

    Systemwide (predominantly Neovim) update

--- config/zsh/fzfcolors.zsh
+++ config/zsh/fzfcolors.zsh
@@ -1,8 +1,8 @@
 # %%base16_template: fzf##default %%
 export FZF_DEFAULT_OPTS='
---color fg:#5B5B5B,bg:#F8F6F4,hl:#A367B3,fg+:#5EAE9B,bg+:#F8F6F4,hl+:#A367B3
---color pointer:#5EAE9B,info:#908D8D,spinner:#4E81A6,header:#908D8D,prompt:#4E81A6,marker:#DD6783
---color border:#3B3B3B
+--color fg:#EBF1FA,bg:#2E3647,hl:#FDAFFF,fg+:#FF92AC,bg+:#2E3647,hl+:#FDAFFF
+--color pointer:#FF92AC,info:#D7E1F5,spinner:#8FFFBC,header:#D7E1F5,prompt:#8FFFBC,marker:#E8FFA6
+--color border:#9ADBFF
 '
 
 # %%base16_template_end%%
