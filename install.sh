55f6d9d16d8a52994c6c9d559a028b64b20506b6
JC-Doyle <doyle.jonty@gmail.com>
2022-03-04 12:38:28 +0200

    several scripts updated

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DOTFOLDER="$HOME/other/dotfiles"
+PKGLIST="$DOTFOLDER/pkglist.txt"
 
 echo "Please ensure you have created a user (a sudoer), and are logged in as that user."
 cd $HOME
@@ -8,20 +9,27 @@ cd $HOME
 
 # Pacman Things
 sudo grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
-sudo sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
+sudo sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
 sudo sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 # Install Yay
-echo "Installing Yay"
+echo "Installing Yay..."
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin 
 makepkg --noconfirm -si
 
 # Cloning Zsh-Plugins
 cd $DOTFOLDER/config/zsh/plugins
+echo "Installing Zsh Plugins..."
 git clone https://github.com/sindresorhus/pure.git
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/jeffreytse/zsh-vi-mode
 
+# Install from pkglist
+echo "Installing Packages..."
 cd $DOTFOLDER
+yay -S --needed $(< $PKGLIST)
+
+# Update config
+$DOTFOLDER/scripts/update-config
