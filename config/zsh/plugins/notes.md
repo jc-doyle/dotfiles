bb75c60d9dab1f4b25098e22d51261dc1852989d
JC-Doyle <doyle.jonty@gmail.com>
2022-03-04 02:00:44 +0200

    notes included

---
+++ config/zsh/plugins/notes.md
@@ -0,0 +1,4 @@
+## Replace CUTBUFFER in zvm_vi_yank()
+```
+   printf %s "${CUTBUFFER}" | xclip -sel c
+```
--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
+DOTFOLDER="$HOME/other/dotfiles"
+
 echo "Please ensure you have created a user (a sudoer), and are logged in as that user."
 cd $HOME
 
 # Set Config
 
 # Pacman Things
-grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
-sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
-sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+sudo grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+sudo sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
+sudo sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 # Install Yay
 echo "Installing Yay"
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin 
-sudo makepkg --noconfirm -si
-cd .. && rm -r yay-bin
+makepkg --noconfirm -si
+
+# Cloning Zsh-Plugins
+cd $DOTFOLDER/config/zsh/plugins
+git clone https://github.com/sindresorhus/pure.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/jeffreytse/zsh-vi-mode
+
+cd $DOTFOLDER
