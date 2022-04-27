325aba85bfe6f4d1de868ad7381500c880269d48
JC-Doyle <doyle.jonty@gmail.com>
2022-04-27 13:09:29 +0200

    remove pure newline

--- config/zsh/install.sh
+++ config/zsh/install.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # Cloning Zsh-Plugins
 cd plugins || exit
 rm -rf pure zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-vi-mode
@@ -7,5 +9,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/jeffreytse/zsh-vi-mode
+
+echo "Removing Pure newline..."
+sed -i '/spacious/{n;s/print//}' pure/pure.zsh
 
 mkdir -p $HOME/.cache/zsh/ || exit
